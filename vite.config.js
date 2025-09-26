9b465d171b822a082aaa53f23b661468f74663a2
willymusyoka <willykyeni@gmail.com>
2025-09-26 12:43:48 +0300

    Adds default site URl to LOcal host

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "localhost", // force localhost instead of 127.0.0.1
+    port: 5173,        // explicitly set the port
+  },
 });
